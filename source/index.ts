07be8a88c7e3cd544c47de6b571bd4d854d13edf
Dylan Thomas <dylan.thomas@gmail.com>
2025-12-11 09:53:43 -0600

    formatting

--- source/index.ts
+++ source/index.ts
@@ -1,98 +1,96 @@
 import path from 'node:path';
 import http from 'node:http';
-import { fileURLToPath } from 'node:url';
+import {fileURLToPath} from 'node:url';
 import fs from 'node:fs/promises';
 import getPort from 'get-port';
 import serveHandler from 'serve-handler';
-import { serve } from 'micro';
-import { chromium } from 'playwright';
+import {serve} from 'micro';
+import {chromium} from 'playwright';
 import type {AstroGlobal, AstroIntegration} from 'astro';
 
 // For thumbnails processing
 import fsExtra from 'fs-extra';
-import sharp from "sharp";
+import sharp from 'sharp';
 
 let screenshotFolder = './tmp/screenshots/';
 
 type SelfieOptions = {
-    screen?: { width: number; height: number };
-    viewport?: { width: number; height: number };
+	screen?: {width: number; height: number};
+	viewport?: {width: number; height: number};
 };
 
 export default function selfie(options: SelfieOptions = {}): AstroIntegration {
-    const screen = options.screen || { width: 1024, height: 768 };
-    const viewport = options.viewport || { width: 1024, height: 768 };
-    let root: URL;
-    return {
-        name: 'astro-selfie-veeva',
-        hooks: {
-            // eslint-disable-next-line @typescript-eslint/naming-convention, object-shorthand
-            'astro:config:done': ({ config }) => {
+	const screen = options.screen || {width: 1024, height: 768};
+	const viewport = options.viewport || {width: 1024, height: 768};
+	let root: URL;
+	return {
+		name: 'astro-selfie-veeva',
+		hooks: {
+			// eslint-disable-next-line @typescript-eslint/naming-convention, object-shorthand
+			'astro:config:done': ({config}) => {
 				root = (config as unknown as {root: URL}).root;
-            },
-            // eslint-disable-next-line @typescript-eslint/naming-convention, object-shorthand
-            'astro:build:done': async ({ dir, pages }) => {
-                const screenshotsDir = new URL(screenshotFolder, root);
-                await fs.mkdir(fileURLToPath(screenshotsDir), { recursive: true });
-                const port = await getPort();
-                const baseUrl = new URL(`http://localhost:${port}`);
+			},
+			// eslint-disable-next-line @typescript-eslint/naming-convention, object-shorthand
+			'astro:build:done': async ({dir, pages}) => {
+				const screenshotsDir = new URL(screenshotFolder, root);
+				await fs.mkdir(fileURLToPath(screenshotsDir), {recursive: true});
+				const port = await getPort();
+				const baseUrl = new URL(`http://localhost:${port}`);
 
 				const server = new http.Server(
 					serve(async (request, response) => {
-                    await serveHandler(request, response, {
-                        public: fileURLToPath(dir),
-                    });
+						await serveHandler(request, response, {
+							public: fileURLToPath(dir),
+						});
 					}),
 				);
 
-                server.listen(port);
-                const browser = await chromium.launch();
-                const context = await browser.newContext({
-                    screen,
-                    viewport,
-                });
-                for (const { pathname } of pages) {
-                    const url = new URL(pathname, baseUrl);
-                    const page = await context.newPage();
-                    await page.goto(url.href);
-                    // Wait for 3 seconds
-                    await page.waitForTimeout(3000);
+				server.listen(port);
+				const browser = await chromium.launch();
+				const context = await browser.newContext({
+					screen,
+					viewport,
+				});
+				for (const {pathname} of pages) {
+					const url = new URL(pathname, baseUrl);
+					const page = await context.newPage();
+					await page.goto(url.href);
+					// Wait for 3 seconds
+					await page.waitForTimeout(3000);
 					await page.evaluate('document.body.dataset.astroSelfie = true;');
-                    const screenshot = await page.screenshot({ type: 'png' });
+					const screenshot = await page.screenshot({type: 'png'});
 					const screenshotPath = path.join(
 						fileURLToPath(screenshotsDir),
 						pathname === '' ? 'index.png' : `${pathname}.png`,
 					);
 
-                    await fs.mkdir(path.dirname(screenshotPath), { recursive: true });
-                    await fs.writeFile(screenshotPath, screenshot);
-                }
-                await browser.close();
-                server.close();
+					await fs.mkdir(path.dirname(screenshotPath), {recursive: true});
+					await fs.writeFile(screenshotPath, screenshot);
+				}
+				await browser.close();
+				server.close();
 
-                // Process screenshots into Veeva thumbnails
+				// Process screenshots into Veeva thumbnails
 
-                // Empty current thumbnail directory
-                fsExtra.emptyDirSync('./thumbnails');
+				// Empty current thumbnail directory
+				fsExtra.emptyDirSync('./thumbnails');
 
-                // Get list of all the files in `screenshotFolder`
-                const getImages = (source : string) =>
-                fsExtra.readdirSync(source)
+				// Get list of all the files in `screenshotFolder`
+				const getImages = (source: string) => fsExtra.readdirSync(source);
 
-                const images = getImages(screenshotFolder)
+				const images = getImages(screenshotFolder);
 
-                // Process screenshots into thumbnails
-                images.forEach((image) => {
+				// Process screenshots into thumbnails
+				images.forEach(image => {
+					sharp(screenshotFolder + image)
+						.resize({width: 1024, height: 768})
+						.toFile('./thumbnails/' + path.parse(image).name + '-full.png');
 
-                sharp(screenshotFolder + image)
-                    .resize({ width: 1024, height: 768 })
-                    .toFile("./thumbnails/" + path.parse(image).name + "-full.png")
-
-                sharp(screenshotFolder + image)
-                    .resize({ width: 200, height: 150 })
-                    .toFile("./thumbnails/" + path.parse(image).name + "-thumb.jpg");
-                })
-            },
-        },
-    };
+					sharp(screenshotFolder + image)
+						.resize({width: 200, height: 150})
+						.toFile('./thumbnails/' + path.parse(image).name + '-thumb.jpg');
+				});
+			},
+		},
+	};
 }
